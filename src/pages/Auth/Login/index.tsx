da1a098484dc1cdac7248a5db8a81ed8052728fc
wehevo <lshan0915@outlook.com>
2024-05-15 07:37:34 -0700

    Update login page

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -10,10 +10,32 @@ export default function LoginPage() {
     navigate(`${visitedURL}`);
   }
   return (
-    <>
-      <h1>Login Page</h1>
-      <p>!!!Accessible only without logging in</p>
-      <button onClick={onLogin}>Login</button>
-    </>
+    <div className="bg-gray-100 flex justify-center items-center h-screen">
+      <div className="w-1/2 h-screen hidden lg:block">
+        <img src={require("@/assets/images/png/computer.png")} alt="Placeholder Image" className="object-cover w-full h-full" />
+      </div>
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4 text-gray-700">Login</h1>
+        <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-600"> Username </label>
+            <input type="text" id="username" name="username" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-black" autoComplete="off"/>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-600">Password</label>
+            <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 text-black" autoComplete="off" />
+          </div>
+          <div className="mb-4 flex items-center">
+            <input type="checkbox" id="remember" name="remember" className="text-blue-500" />
+            <label htmlFor="remember" className="text-gray-600 ml-2">Remember Me</label>
+          </div>
+          <div className="mb-6 text-blue-500">
+            <a href="#" className="hover:underline">Forgot Password?</a>
+          </div>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full" onClick={onLogin}>Login</button>
+        <div className="mt-6 text-blue-500 text-center">
+          <a href="#" className="hover:underline">Sign up Here</a>
+        </div>
+      </div>
+    </div>
   );
 }
